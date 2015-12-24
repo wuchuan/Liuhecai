247c000c92b1fd300fe18fba902106361468d519
wuchuan <wuchuan@topwisesz.com>
2015-12-24 10:34:00 +0800

    实现了界面布局

--- app/src/main/java/com/liuhecai/wc/liuhecai/MainActivity.java
+++ app/src/main/java/com/liuhecai/wc/liuhecai/MainActivity.java
@@ -1,13 +1,59 @@
 package com.liuhecai.wc.liuhecai;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends Activity {
+
+    private  LinearLayout MainLearnLayout;
+    private ArrayList<CheckBox>  AllCheckBox =new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MainLearnLayout = (LinearLayout)findViewById(R.id.main_learn);
+        initView();
     }
+
+    private void initView() {
+        for(int i=0;i<MainLearnLayout.getChildCount();i++)
+        {
+            if(MainLearnLayout.getChildAt(i)instanceof LinearLayout){
+                for(int j=0;j<MainLearnLayout.getChildCount();j++)
+                {
+                    if(MainLearnLayout.getChildAt(j)instanceof CheckBox){
+                        AllCheckBox.add((CheckBox) MainLearnLayout.getChildAt(j));
+                    }
+                    if(MainLearnLayout.getChildAt(j)instanceof LinearLayout){
+                        for(int t=0;t<((LinearLayout) MainLearnLayout.getChildAt(j)).getChildCount();t++)
+                        {
+                            if(((LinearLayout) MainLearnLayout.getChildAt(j)).getChildAt(t)instanceof CheckBox){
+                                AllCheckBox.add((CheckBox) ((LinearLayout) MainLearnLayout.getChildAt(j)).getChildAt(t));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initViewDeful();
+    }
+
+    private void initViewDeful() {
+        for(CheckBox i:AllCheckBox){
+            i.setChecked(true);
+        }
+
+    }
+
+
 }
